d2b132158e2e3e66b1a58f38e9355d460a8446e3
H3NRY33 <37905046+H3NRY33@users.noreply.github.com>
2018-10-26 16:58:58 -0400

    80% COMPLETE. NEEDS SOME POLISHING...

--- K_TRACK.cpp
+++ K_TRACK.cpp
@@ -2,31 +2,23 @@
 #include <vector>
 #include <fstream>
 #include <string>
+#include <sstream>
 using namespace std; 
-/* Few functions that reads text files and adds new information to them... WOP 
-B: 10/25/18 */
-
-void addtoSmall(string inp,vector<char>& small2){//takes a string and a vector to change said vector such that every char in the string is now an element of the vector UPDATE: Should now CONVERT files with '.' and '0' INTO BOOLEAN when pushing back.
-    for (size_t i=0; i<=inp.length(); i++){
-    small2.push_back(inp[i]);
-	}
-}
 
 
-void readFile(const string fname,const string identification,vector<vector<char> > &big)//Takes a string, and a vector of vectors
+void readFile(const string fname,vector<vector<string> > &big)//Takes a string, and a vector of vectors
 {
   big.clear();
-  vector<char> small;
+  vector<string> small;
   string Rinp;
   ifstream myfile(fname);// creates and object called myfile with the file path
   if(myfile.is_open()){
     while(getline(myfile,Rinp)){//While myfile is being read into string Rinp
-        if(Rinp=="--idend"+identification){
+       small.push_back(Rinp);
+        big.push_back(small);//Takes the vector we inputted and creates an element containing small(a vector of chars)
+         if(Rinp.length()==0){
           break;
           }
-        
-        addtoSmall(Rinp,small);//Takes string Rinp and vector small, and then makes small contain every char of Rinp as a element of small
-        big.push_back(small);//Takes the vector we inputted and creates an element containing small(a vector of chars)
         small.clear();//clears all elements from small
       }
   myfile.close();
@@ -34,34 +26,39 @@ void readFile(const string fname,const string identification,vector<vector<char>
 }
 
 
-void editData(const string id, string fname , string lname , string dob,vector<vector<char> > &edit){
-	string checkLine;
-	bool dataFound = false;
-	size_t lEdit = 0;
-	size_t i;
+void editData(string id, string fname,string lname,string dob, vector<vector<string> > &edit){
+	size_t cell;
+	size_t cell_Access;
+	stringstream conv(id);
+	conv >> cell;
+	cell_Access = (cell*5)-4;
+	vector<string> title{"fname:","lname:","birth:"};
+	vector<string> data{fname,lname,dob};
+	vector<string> temp1;
+	vector<string> temp2;
+	vector<string> insertVec;
 	
 	
-	for ( i=0; i<edit.size(); i++){
-		checkLine.clear();
+	for (size_t i = cell_Access; i<cell_Access+3; i++){
+	edit[i].clear();
+	temp1.clear();
+	temp2.clear();
+	insertVec.clear();
+	temp1.push_back(title[i-cell_Access]);
+	temp2.push_back(data[i-cell_Access]);
 
-			if (dataFound == true){
-		
-		}
-		if (dataFound == false){
-			for (size_t j=0 ; j<edit[i].size(); j++){
-            checkLine+=edit[i][j];
-            }
-        if(checkLine =="ident:"+id) dataFound = true;	
-		}
-      
+	insertVec.reserve( temp1.size() + temp2.size() ); // preallocate memory
+	insertVec.insert( insertVec.end(), temp1.begin(), temp1.end() );
+	insertVec.insert( insertVec.end(), temp2.begin(), temp2.end() );
+	
+	edit[i]= insertVec;
 	
 	}
 	
 }
 
 
-
-void outFile(const string fname,vector<vector<char> > vec){//Takes a string(Most likely file location) and a vector of vectors
+void outFile(const string fname,vector<vector<string> > vec){//Takes a string(Most likely file location) and a vector of vectors
     ofstream myfile;//Creates object myfile to be written
     myfile.open(fname);//Opens fname, which would be the file path
     for (size_t i=0; i<vec.size(); i++){
@@ -77,28 +74,31 @@ void outFile(const string fname,vector<vector<char> > vec){//Takes a string(Most
 
 int main(){
 	
-	vector <vector<char> > mainVec;
+	vector <vector<string> > mainVec;
 	//Asks what file you want to look at
-	string fileName;
+	string fileName ="data$.txt";
 	//Asks what up to what id number you want to look at
 	string idAccess;
-	
-	
-	cout<<"Enter name of file desired to read\n";
-	cin >> fileName;
-	fileName+=".txt";
+
+
 	cout<<"Enter up to what id to access\n";
 	cin >> idAccess;
 	
-	readFile(fileName,idAccess,mainVec);
-	editData(idAccess,"hugh" ,"mungus","11/16/1973",mainVec);
+	string inp1="Hugh";
+	string inp2="Mungus";
+	string inp3="11/09/1987";
 	
+	readFile(fileName,mainVec);
+	editData(idAccess,inp1,inp2,inp3,mainVec);
+	outFile(fileName,mainVec);
+
 	 for (size_t i =0; i<mainVec.size(); i++){
         for (size_t j=0 ; j<mainVec[i].size(); j++){
             cout << mainVec[i][j];
             }
             cout << "\n";
         }
+        
 	
 	
 	
