f5087d956bc953288e77b7ffb00a3a8a28e7aae2
H3NRY33 <37905046+H3NRY33@users.noreply.github.com>
2018-10-28 01:42:22 -0400

    CHANGED TO REGULAR VECTORS
    MUCH BETTER NOW

--- K_TRACK.cpp
+++ K_TRACK.cpp
@@ -5,102 +5,86 @@
 #include <sstream>
 using namespace std; 
 
-
-void readFile(const string fname,vector<vector<string> > &big)//Takes a string, and a vector of vectors
-{
-  big.clear();
-  vector<string> small;
+void readFile(const string fname,vector<string> &editVec){
+  editVec.clear();
   string Rinp;
-  ifstream myfile(fname);// creates and object called myfile with the file path
+  ifstream myfile(fname);
   if(myfile.is_open()){
-    while(getline(myfile,Rinp)){//While myfile is being read into string Rinp
-       small.push_back(Rinp);
-        big.push_back(small);//Takes the vector we inputted and creates an element containing small(a vector of chars)
+    while(getline(myfile,Rinp)){
+	
+       	editVec.push_back(Rinp);
          if(Rinp.length()==0){
           break;
           }
-        small.clear();//clears all elements from small
       }
   myfile.close();
   }
 }
-
-
-void editData(string id, string fname,string lname,string dob, vector<vector<string> > &edit){
-	size_t cell;
-	size_t cell_Access;
-	stringstream conv(id);
-	conv >> cell;
-	cell_Access = (cell*5)-4;
-	vector<string> title{"fname:","lname:","birth:"};
+void createData(size_t dSize,vector<string> &editVec, vector<string> title){
+	editVec.clear();
+	string id;
+	for(size_t i = 1 ; i <=dSize;i++){
+	id = to_string(i);
+	editVec.push_back("IDENTIFICATION: " + id);
+		for(size_t j = 0 ; j<3 ; j++){
+		editVec.push_back(title[j]);
+		}
+	editVec.push_back("------------------------------");
+	}
+}
+void editData(size_t cell, string fname,string lname,string dob,vector<string> &editVec,vector<string> title){
+	
+	size_t cell_Access = (cell*5)-4;
 	vector<string> data{fname,lname,dob};
-	vector<string> temp1;
-	vector<string> temp2;
-	vector<string> insertVec;
-	
-	
-	for (size_t i = cell_Access; i<cell_Access+3; i++){
-	edit[i].clear();
-	temp1.clear();
-	temp2.clear();
-	insertVec.clear();
-	temp1.push_back(title[i-cell_Access]);
-	temp2.push_back(data[i-cell_Access]);
 
-	insertVec.reserve( temp1.size() + temp2.size() ); // preallocate memory
-	insertVec.insert( insertVec.end(), temp1.begin(), temp1.end() );
-	insertVec.insert( insertVec.end(), temp2.begin(), temp2.end() );
-	
-	edit[i]= insertVec;
-	
+	for (size_t i = cell_Access; i<cell_Access+3; i++){
+		editVec[i].clear();
+		editVec[i] = title[i-cell_Access] + data[i-cell_Access];
 	}
 	
 }
-
-
-void outFile(const string fname,vector<vector<string> > vec){//Takes a string(Most likely file location) and a vector of vectors
+void outFile(const string fname,vector<string> big){//Takes a string(Most likely file location) and a vector of vectors
     ofstream myfile;//Creates object myfile to be written
     myfile.open(fname);//Opens fname, which would be the file path
-    for (size_t i=0; i<vec.size(); i++){
-        for (size_t j=0 ; j<vec[i].size(); j++){
-            myfile << vec[i][j];//Writes every char in the file chosen into a vector of vectors
-            }
-            myfile << endl;
+    for (size_t i=0; i<big.size(); i++){
+        myfile << big[i]<<"\n";
         }
         myfile.close();
 }
 
-
-
 int main(){
 	
-	vector <vector<string> > mainVec;
-	//Asks what file you want to look at
+	vector<string> descr{"FIRST NAME: ","LAST NAME: ","DOB: "};
+	vector<string> mainVec;
+	string inp1="0000000000000000000";
+	string inp2="0000000000000000000";
+	string inp3="0000000000000000000";
 	string fileName ="data$.txt";
-	//Asks what up to what id number you want to look at
-	string idAccess;
-
-
-	cout<<"Enter up to what id to access\n";
+	size_t cDatabase;
+	size_t idAccess;
+		
+	cout<<"How many spots to make....\n";
+	cin >> cDatabase;	
+	createData(cDatabase,mainVec,descr);
+	outFile(fileName,mainVec);
+	cout<<"Wher would you like to edit?\n";
 	cin >> idAccess;
 	
-	string inp1="Hugh";
-	string inp2="Mungus";
-	string inp3="11/09/1987";
-	
 	readFile(fileName,mainVec);
-	editData(idAccess,inp1,inp2,inp3,mainVec);
-	outFile(fileName,mainVec);
-
+	editData(idAccess,inp1,inp2,inp3,mainVec,descr);
+	outFile(fileName,mainVec);	
+		
+/*	readFile(fileName,mainVec);
+	cout<<"Before\n";
 	 for (size_t i =0; i<mainVec.size(); i++){
-        for (size_t j=0 ; j<mainVec[i].size(); j++){
-            cout << mainVec[i][j];
-            }
-            cout << "\n";
+            cout << mainVec[i]<<"\n";
         }
-        
-	
-	
-	
+	editData(idAccess,inp1,inp2,inp3,mainVec,descr);
+    outFile(fileName,mainVec);
+    
+    cout<<"After\n"; 
+        for (size_t j =0; j<mainVec.size(); j++){
+            cout << mainVec[j]<<"\n";
+        }*/	
 	return 0; 
 }
