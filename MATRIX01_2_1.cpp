969e95ab3bd73c7ba99bf9ee28dd4fd639a88be1
H3NRY33 <37905046+H3NRY33@users.noreply.github.com>
2018-11-10 15:05:07 -0500

    ADDED COMMENTS, INT MAIN NEEDS TUNING.

--- MATRIX01_2_1.cpp
+++ MATRIX01_2_1.cpp
@@ -9,92 +9,93 @@ using namespace std;
 
 
 
-//Function that creates a vector a specific length
+//Function that creates a vector of ints with contents of i value.
 void createAxis(const int length, vector<int> &V){
 	for (int i = 0 ; i <=length ; i++){
 		V.push_back(i);
 	}
 }//END createAxis
 
-
+//Creates an empty 2D matrix of type bool. There are Y amount of vectors of size X.
 void createMatrix(const int X, const int Y , vector<vector<bool> > &matrix){
+	//Creates a Row of bools of length X.
 	vector<bool> Row;
-
-	for (int i = 0 ; i <=X ; i++){
+	for (int i = 0 ; i <= X ; i++){
 		Row.push_back(false);
 	}
-	for (int j = 0 ; j<= Y ;j++){
+	//Pushes the Row Vectos Y amount of times.
+	for (int j = 0 ; j <= Y ;j++){
 		matrix.push_back(Row);
 	}
 }//END createMatrix
 
-
-
+//Vector used to store x and y coordinates for comparison to actual point given by the user.
 void feedtoVec(const int position,const vector<int> inpV, vector<int> &compareV ){
 	compareV.clear();
 	compareV.push_back(inpV[position]);
 	compareV.push_back(inpV[position + 1]);
-}//END feedtoVec
+}//END feedtoVec.
 
-
-
+//Takes a 2D Vector along with user coordinates. It checks to see if it exists within the X and Y axis points.
 void editMatrix(const vector<int> xAxis,const vector<int> yAxis, const vector<int> plotV, vector<vector<bool> > &matrix){
+//Vector that store's a single coordinate from the user.
 vector<int> compareVec;
-
+	//Iterates through the list of user's coordinates.
 	for (int i = 0 ; i < plotV.size() ; i+=2){
 	feedtoVec(i,plotV,compareVec);
-
+		//Iterates through Y axis points.
 		for(int j = 0 ; j < yAxis.size(); j++){
+			//Iterates through X axis points.
 			for(int k = 0; k < xAxis.size(); k++){
+				//Logic for coordinate comparison.
 				if((compareVec[0] == xAxis[k]) && (compareVec[1] == yAxis[j])){
 				matrix[j][k] = true;
 				}
-			
 			}
 		}
-
 	}
 }//END editMATRIX
 
 void printMatrix(const vector<vector<bool> > Pmatrix){
 	int j = 1;
-	cout<<"   ";
-	for (int l =0; l < Pmatrix.size(); l++){
+	//Prints out X axis of Matrix.
+	cout<<"    ";
+	for (int l =0; l < Pmatrix[0].size(); l++){
 		if(l < 10){
 		cout<<l<<"  ";
 		}
 		else{
 		cout<<l<<" ";
 		}
-		
 	}
 	cout<<"\n";
-	cout<<"   ";
-	for (int k =0; k < Pmatrix.size(); k++){
+	cout<<"    ";
+	//Used for X axis Aesthetics.
+	for (int k =0; k < Pmatrix[0].size(); k++){
+		if(k < 10){
 		cout<<"-  ";
+		}
+		else{
+		cout<<"-  ";
+		}
+		
 	}
+	//Prints out Y axis.
 	cout<<"\n";
 	for (int i = 0 ; i <Pmatrix.size(); i++){
 		if(i<10){
-		cout<<i<<" |";
+		cout<<i<<" | ";
 		}
 		else{
-		cout<<i<<"|";
+		cout<<i<<"| ";
 		}
-		
-
-
+		//Prints out the actual values of the Matrix
 		for (j = 0 ; j<Pmatrix[i].size();j++){
 			cout<<Pmatrix[i][j]<<"  ";
 		}
 		cout<<"\n\n";
 	}
 } //END print matrix
-
-
-
-
-
 
 
 
@@ -118,7 +119,6 @@ createAxis(intervalX,xAxis);
 createAxis(intervalY,yAxis);
 createMatrix(intervalX,intervalY,gridMatrix);
 
-
 cout<<"Insert Coordinates in format X Y \n";
 
 while(cin>>plotPoint){
@@ -130,15 +130,5 @@ while(cin>>plotPoint){
 editMatrix(xAxis,yAxis,plotVector,gridMatrix);
 printMatrix(gridMatrix);
 
-
-
-
-
-
-
-
-
-
-	
 	return 0;
 }
