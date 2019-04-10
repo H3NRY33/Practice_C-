7d1681cc39fbb1d605545644b7be36841557c328
Henry_Puma <pumahenry@live.com>
2019-04-10 12:37:28 -0400

    new_proj

---
+++ LIST_TO_STRING.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+/*Idea: take a list of words from user input OR a list.txt file ... and return them as strings ... */
+
+
+int main(int argc, char *argv[]){
+// vector<string> listEdit;
+// string input;
+// while(cin >> input){
+// if(input == "1"){break;}
+// string editStr = "\""+input+"\",";
+// listEdit.push_back(editStr);
+
+// }
+// cout<<"HERE IS STRING LIST"<<endl;
+// for (size_t i = 0; i < listEdit.size() ; i++){
+//     cout<<listEdit[i]<<endl;
+// }
+return 0;
+}
+
+
