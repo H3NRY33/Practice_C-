5ea9ebbc8d68a69722807323737b47486d7acb0c
Henry_Puma <pumahenry@live.com>
2019-06-07 23:25:20 -0400

    calc203_WIP

--- CALC203/calc203.cpp
+++ CALC203/calc203.cpp
@@ -8,8 +8,9 @@ calc203<Item>::calc203(){
     maxSize = DEF_SIZE;
 }
 template <class Item>
-calc203<Item>::calc203(size_t numVectors,size_t dimenVector=DEF_SIZE){
-    maxSize = dimenVector;
+calc203<Item>::calc203(size_t numVectors,size_t vectorDimension = DEF_SIZE){
+    maxSize = vectorDimension;
+    previousAnswer(vectorDimension);
     for(size_t i = 0; i < numVectors; i++){
         vector<Item> myVector(dimenVector);
         vectorList.push_back(myVector);
@@ -21,17 +22,33 @@ template <class Item>
 calc203<Item>::calc203(const calc203<Item>& Source){
     this->vectorList = Source.vectorList;
     this->maxSize = Source.maxSize;
+    this->previousAnswer = Source.previousAnswer;
 }
 
 // MODIFICATION MEMBER FUNCTIONS.
 template <class Item>
 void calc203<Item>::setVector(size_t vectorNum,vector<Item> inputVector){
-    if(vectorList.size() > vectorNum){
+    if(vectorList.size() > vectorNum && inputVector.size() == maxSize){
         vectorList[vectorNum] = inputVector;
     }
     else{
-        cout<<"VECTOR INDEX IS NOT VALID\n";
+        cout<<"VECTOR INDEX/SIZE IS NOT VALID\n";
     }
+}
+template<class Item> 
+void calc203<Item>::addVector(vector<Item> inputVector){
+    if(inputVector.size() == maxSize){
+        vectorList.push_back(inputVector);
+    }
+    else{
+        cout<<"INPUT VECTOR DOES NOT MEET THE DIMENSION REQUIREMENTS!\n";
+    }
+}
+template<class Item> 
+void calc203<Item>::clearData(){
+    vectorList.clear();
+    previousAnswer.clear();
+    maxSize = DEF_SIZE;
 }
 
 // CONSTANT MEMBER FUNCTIONS.
@@ -50,6 +67,11 @@ size_t calc203<Item>::vecCount() const{
     return vectorList.size();
 }
 
+template<class Item> 
+vector<Item> calc203<Item>::getPrevious() const{
+return previousAnswer;
+}
+
 template <class Item>
 void calc203<Item>::printVector(size_t vectorNum) const{
     if(vectorList.size() > vectorNum){
@@ -60,50 +82,93 @@ void calc203<Item>::printVector(size_t vectorNum) const{
     }
 }
 
-
 // CALC FUNCTIONS.
 template <class Item>
 vector<Item> calc203<Item>::addVec(size_t indexA=0, size_t indexB=1){
-    vector<Item> ansVec(maxSize);
-    if(indexA >=0 && indexB < vectorList.size()){
-        if(vectorList[indexA].size() == vectorList[indexB].size()){
+    vector<Item> vecSum(maxSize);
+    if((vectorList.size() > indexA && vectorList.size() > indexB) && vectorList.size() >= 2){
+        if(sameSize(vectorList[indexA],vectorList[indexB])){
             for(size_t i = 0; i < maxSize; i++){
                 ansVec[i]+=vectorList[indexA][i] + vectorList[indexB][i];
             }
         }
     }
-    return ansVec;
+    if(!vecSum.empty()){
+        previousAnswer = vecSum;
+    }
+    return vecSum;
 }
 template <class Item>
 vector<Item> calc203<Item>::addAll(){
     vector<Item> totalSum(maxSize);
     size_t listSize = vectorList.size();
-    if(!vectorList.empty()){
+    if(sameDimension(vectorList)){
         for(size_t i = 0; i < listSize; i++){
             for(size_t j = 0; j < maxSize; j++){
                 totalSum[j]+=vectorList[i][j];
             }
         }
     }
+    if(!totalSum.empty()){
+        previousAnswer = totalSum;
+    }
     return totalSum;
 }
 
 template <class Item>
 vector<Item> calc203<Item>::subVec(size_t indexA=0, size_t indexB=1){
-    
+    vector<Item> vecSub(maxSize);
+    if(vectorList.size() > indexA && vectorList.size() > indexB){    
+        if(sameSize(vectorList[indexA],vectorList[indexB])){
+            for(size_t i = 0; i < maxSize; i++){
+                vecSub[i] = vectorList[indexA][i] - vectorList[indexB][i];
+            }
+        }
+    }
+    if(!vectorSub.empty()){
+        previousAnswer = vecSub;
+    }
+    return vecSub;
 }
 
 template <class Item>
-vector<Item> calc203<Item>::multVec(size_t indexA=0, size_t indexB=1){}
+vector<Item> calc203<Item>::multVec(size_t indexA=0, size_t indexB=1){
+    vector<Item> vectorMult(maxSize);
+     if(vectorList.size() > indexA && vectorList.size() > indexB){
+         if(sameSize(vectorList[indexA],vectorList[indexB]){
 
+         }
+
+     }
+}
+// NOTE: NEED A SPECIAL CASE WHEN AN ELEMENT HAS A 0.
 template <class Item>
-vector<Item> calc203<Item>::multAll(size_t indexA=0, size_t indexB=1){}
+vector<Item> calc203<Item>::multAll(){
+    vector<Item> vecMult(maxSize);
+    if(vectorList.size() >= 2){
+        if(sameDimension(vectorList)){
+            size_t listSize = vectorList.size();
+            for(size_t i =0; i < listSize; i++){
+                for(size_t j = 0; j < maxSize; j++){
+                    vecMult[j]*=vectorList[i][j];
+                }
+            }
+        }
+    }
+    if(!vecMult.empty()){
+        previousAnswer = vecMult;
+    }
+    return vecMult;
+}
 
 template <class Item>
 vector<Item> calc203<Item>::scaleVec(size_t vectorIndex, int scalar=1){}
 
 template <class Item>
 vector<vector<Item> > calc203<Item>::scaleAll(int scalar=1){}
+
+template <class Item> 
+vector<Item> calc203<Item>::cross(size_t indexA, size_t indexB){}
 
 
 
--- CALC203/calc203.h
+++ CALC203/calc203.h
@@ -6,14 +6,14 @@
 #include <cmath>
 
 using namespace std;
-// NOTE: THIS IS MEANT TO WORK WITH SIGNED NUMBERS!
+// NOTE: THIS IS MEANT TO WORK WITH SIGNED TYPES!
 template<class Item>
 class calc203{
     static const size_t DEF_SIZE = 3;
     public:
     // CONSTRUCTOR
     calc203();
-    calc203(size_t numVectors,size_t dimenVector=DEF_SIZE);
+    calc203(size_t numVectors,size_t vectorDimension = DEF_SIZE);
 
     // COPY CONSTRUCTOR
     calc203(const calc203<Item>& Source);
@@ -21,30 +21,31 @@ class calc203{
 
     // MODIFICATION MEMBER FUNCTIONS.
     void setVector(size_t vectorNum,vector<Item> inputVector);
+    void addVector(vector<Item> inputVector);
+    void clearData();
 
     // CONSTANT MEMBER FUNCTIONS.
     vector<Item> getVector(size_t vectorNum) const;
-
-    size_t vecCount() const;
-
+    vector<Item> getPrevious() const;
     void printVector(size_t vectorNum) const;
+    size_t vecCount() const;;
 
     // CALC FUNCTIONS.
     vector<Item> addVec(size_t indexA=0, size_t indexB=1);
     vector<Item> addAll();
 
     vector<Item> subVec(size_t indexA=0, size_t indexB=1);
-    vector<Item> subAll(size_t indexA=0, size_t indexB=1);
 
     vector<Item> multVec(size_t indexA=0, size_t indexB=1);
-    vector<Item> multAll(size_t indexA=0, size_t indexB=1);
+    vector<Item> multAll());
 
     vector<Item> scaleVec(size_t vectorIndex, int scalar=1);
     vector<vector<Item> > scaleAll(int scalar=1);
-
+    vector<Item> cross(size_t indexA, size_t indexB);
 
     private:
     vector<vector<Item> > vectorList;
+    vector<Item> previousAnswer;
     size_t maxSize;
 
     template <class Item>
@@ -60,6 +61,24 @@ class calc203{
             }
         }
         cout<<"}\n";
+    }
+    template <class Item> 
+    bool sameSize(vector<Item> vector1, vector<Item> vector2){
+        return((vector1.size() == maxSize) && (vector2.size() == maxSize));
+    }
+
+    template<class Item>
+    bool sameDimension(vector<vector<Item> > vectorCheck){
+        if(!vectorCheck.empty()){
+            size_t vectorSize = vectorCheck.size();
+            for(size_t i = 0; i < vectorSize; i++){
+                if(vectorCheck[i].size() != maxSize){
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
     }
 };
 #endif
